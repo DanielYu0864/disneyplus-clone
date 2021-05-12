28321da85994b5bdcd035f2d3c5a6cb04b012281
Daniel.Yu <adam741963@gmail.com>
2021-05-11 17:48:22 -0700

    ADD: header

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # disneyplus-clone
-* the clone of disney plus
-* built with React JS
-* shout out to Clever Programmer
+
+- the clone of disney plus
+- built with React JS
+- shout out to [Clever Programmer](https://www.youtube.com/watch?v=kn8HPNwPEkw&t=193s)
---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
+
+/*
+todo 1: nav bar(header)
+todo 2: slider
+todo 3: viewer
+todo 4: move list
+*/
