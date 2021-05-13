622eb32303ec54ddbebf6eaa67382e7f35dd6ea2
Daniel.Yu <adam741963@gmail.com>
2021-05-13 16:54:17 -0700

    ADD: Detail

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './App.css';
+import Detail from './components/Detail';
 import Header from './components/Header';
 import Home from './components/Home';
 
 function App() {
   return (
     <div className='App'>
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path='/detail'>
+            <Detail />
+          </Route>
+          <Route path='/' exact>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
