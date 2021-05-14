0686c2e95fb8f7863b14947e0784ee4d674e2b64
Daniel.Yu <adam741963@gmail.com>
2021-05-14 12:41:29 -0700

    ADD: grab movies from DB

--- frontend/src/components/Movies.js
+++ frontend/src/components/Movies.js
@@ -1,35 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
+import { selectMovies } from '../features/movies/movieSlice';
+import { useSelector } from 'react-redux';
 
 const Movies = () => {
+  const movies = useSelector(selectMovies);
+
+  console.log(movies);
+
   return (
     <Container>
       <h4>Recommended for You</h4>
       <Content>
-        <Wrap>
-          <img src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg' />
-        </Wrap>
-        <Wrap>
-          <img src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg' />
-        </Wrap>
-        <Wrap>
-          <img src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg' />
-        </Wrap>
-        <Wrap>
-          <img src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg' />
-        </Wrap>
-        <Wrap>
-          <img src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg' />
-        </Wrap>
-        <Wrap>
-          <img src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg' />
-        </Wrap>
-        <Wrap>
-          <img src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg' />
-        </Wrap>
-        <Wrap>
-          <img src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg' />
-        </Wrap>
+        {movies ? (
+          movies.map((movie, key) => (
+            <Wrap key={key}>
+              <img src={movie.cardImg} />
+            </Wrap>
+          ))
+        ) : (
+          <div>no data</div>
+        )}
       </Content>
     </Container>
   );
@@ -43,6 +34,7 @@ const Content = styled.div`
   display: grid;
   grid-gap: 20px;
   grid-template-columns: repeat(4, minmax(0, 1fr));
+  padding-bottom: 20px;
 `;
 
 const Wrap = styled.div`
@@ -56,7 +48,7 @@ const Wrap = styled.div`
 
   img {
     width: 100%;
-    hiehgt: 100%;
+    height: 100%;
     object-fit: cover;
   }
 
